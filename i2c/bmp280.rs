81ac7bf08077bbf2e445ebdab01e967377355662
iagoscm <iagomatosousa@gmail.com>
2025-01-07 20:05:14 -0300

    corrige nome bmp

--- i2c/bmp280.rs
+++ i2c/bmp280.rs
@@ -1,19 +1,19 @@
-use bme280::{i2c::BME280 as Device, Error, Measurements};
+use bmp280::{i2c::BMP280 as Device, Error, Measurements};
 use linux_embedded_hal::{Delay, I2CError, I2cdev};
 
-pub struct Bme280 {
+pub struct Bmp280 {
     delay: Delay,
     device: Device<I2cdev>,
 }
 
-impl Bme280 {
+impl Bmp280 {
     pub fn new() -> Self {
         let i2c = I2cdev::new("/dev/i2c-1").unwrap();
         let mut device = Device::new_primary(i2c);
         let mut delay = Delay;
         device.init(&mut delay).unwrap();
 
-        Bme280 { delay, device }
+        Bmp280 { delay, device }
     }
 
     pub fn measure(&mut self) -> Result<Measurements<I2CError>, Error<I2CError>> {
