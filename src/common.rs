e9047520b6c5c717c8d7da7c8bc1a35dc6fcf291
iagoscm <iagomatosousa@gmail.com>
2025-01-15 23:13:51 -0300

    Revert "tentativa de fazer o modbus; mantem versao antiga"
    This reverts commit 335c85c0138b1974e9c7d5d4076f684616017a28.

--- src/common.rs
+++
@@ -1,8 +0,0 @@
-#[derive(Clone, Copy, PartialEq, Eq, Debug)]
-#[repr(u8)]
-pub enum Car {
-    Seta = 0x00,
-    Cruise = 0x01,
-    Farol = 0x02;
-}
-
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-//mod i2c;
+mod i2c;
 mod uart;
 mod gpio;
 
-use crate::uart::modbus::{create_modbus, read_modbus, LE_TEMP, CONTROL_SETA_ESQUERDA, CONTROL_SETA_DIREITA};
-use std::thread;
-use uart::modbus::seta_esquerda;
-use uart::modbus::temp_motor;
-use std::time::Duration;
+//use std::thread;
+//use uart::modbus::seta_esquerda;
+//use uart::modbus::temp_motor;
+//use std::time::Duration;
 
 fn main() {
   // a cada 50ms pedir temperatura do motor, e ler comando das setas
@@ -16,42 +15,4 @@ fn main() {
     //thread::sleep(Duration::from_millis(50));
   //}
   gpio::pisca();
-/*  loop {
-    let temp_motor_data = create_modbus(LE_TEMP, &[]);
-    let seta_esquerda_data = create_modbus(CONTROL_SETA_ESQUERDA, &[1]);
-    let seta_direita_data = create_modbus(CONTROL_SETA_DIREITA, &[1]);
-
-    let response_temp = read_modbus(LE_TEMP, &temp_motor_data);
-    let response_seta_esquerda = read_modbus(CONTROL_SETA_ESQUERDA, &seta_esquerda_data);
-    let response_seta_direita = read_modbus(CONTROL_SETA_DIREITA, &seta_direita_data);
-
-    match response_temp {
-        Ok(response) => {
-            println!("Temperatura do motor: {:?}", response);
-        }
-        Err(e) => {
-            println!("Erro ao ler temperatura: {}", e);
-        }
-    }
-
-    match response_seta_esquerda {
-        Ok(response) => {
-            println!("Comando de seta à esquerda recebido: {:?}", response);
-        }
-        Err(e) => {
-            println!("Erro ao ler comando de seta à esquerda: {}", e);
-        }
-    }
-
-    match response_seta_direita {
-        Ok(response) => {
-            println!("Comando de seta à direita recebido: {:?}", response);
-        }
-        Err(e) => {
-            println!("Erro ao ler comando de seta à direita: {}", e);
-        }
-    }
-
-    std::thread::sleep(std::time::Duration::from_millis(5000));
-}*/
 }
